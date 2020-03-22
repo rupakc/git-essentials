1dbfca7aa583b422c7bdd5f498d9ae0d410618e2
Rupak Chakraborty <rupak97.4@gmail.com>
2020-03-22 11:43:00 +0100

    Resolved merge from b_branch

--- test.py
+++ test.py
@@ -4,5 +4,7 @@ print("Hello Berlin")
 print("welcome to the new era and dawn of a new age")
 print("here we go again")
 print("this is commit from branch b")
-
-
+print("B Branch Rebase commit")
+print("Tring git rebase")
+print(" Oh yes trying it")
+print("And again trying it")
